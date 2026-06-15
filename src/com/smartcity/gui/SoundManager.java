9a78f7781418edcba68598d2c43aed2c5da25525
supremekaiser5 <orientalsteelwarrior123@gmail.com>
2026-06-15 11:23:17 +0700

    gui

---
+++ src/com/smartcity/gui/SoundManager.java
@@ -0,0 +1,22 @@
+package com.smartcity.gui;
+
+import javafx.scene.media.AudioClip;
+
+public class SoundManager {
+
+    public void playHorn() {
+
+    }
+
+    public void playSiren() {
+
+    }
+
+    public void playTurnSignal() {
+
+    }
+
+    public void stopAllSounds() {
+
+    }
+}
